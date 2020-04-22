087c14fef1a3d899bed83d64eb8ee71f491c96ab
Alan Willoughby <satyadvaya@gmail.com>
2020-04-22 14:38:16 -0700

    commit to update hike list and helper functions

--- hike-list/hike-list.js
+++ hike-list/hike-list.js
@@ -1,3 +1,6 @@
+import hikes from '../data/data.js';
+import findByQuadrant from '../common/findByQuadrant.js';
+import renderSpecificHike from './renderSpecificHike.js';
 
 const serchId = new URLSearchParams(window.location.search);
 
@@ -5,3 +8,24 @@ const keyID = serchId.get('userId');
 const localVar = localStorage.getItem(keyID);
 
 console.log(localVar);
+
+const quadrant = 'Northeast';
+
+const name = document.getElementById('quadrant');
+
+name.textContent = quadrant;
+
+// On click, all hikes which share that quadrant's ID
+
+// find hikes matching quadrant through some type of findById function
+
+const filter = findByQuadrant(quadrant, hikes);
+
+const listDest = document.getElementById('quadrant-hikes');
+
+// will be rendered onto that quadrant's hikes page.
+
+filter.forEach(hike => {
+    const hikeRender = renderSpecificHike(hike);
+    listDest.append(hikeRender);
+});
