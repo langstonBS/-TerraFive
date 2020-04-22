6dca5855979d2853772fc63fbea32826eb747723
karlwitek <karl.witek20@gmail.com>
2020-04-22 13:47:01 -0700

    pass three tests

---
+++ test/add-local-storage-test.js
@@ -0,0 +1,28 @@
+// IMPORT MODULES under test here:
+// import example from '../src/example.js';
+import addLocalStorage from '../common/addLocalStorage.js';
+
+
+
+const test = QUnit.test;
+
+test('test to check if a key and object is added to local storage', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const user = {
+        name: 'username',
+        id: 'test123',
+        img: 0,
+        favorites:{ },
+    };
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const expected = '{"name":"username","id":"test123","img":0,"favorites":{}}'
+    addLocalStorage(user.id, user);
+    const result = localStorage.getItem('TEST123');
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(expected, result);
+});
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,6 @@
-import './example.test.js';
+//import './example.test.js';
+import './add-local-storage-test.js';
+import './create-user-test.js';
+import './renderAttractions-test.js';
+
+
