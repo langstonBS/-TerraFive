b41dfe1c52f527d5d106020d0edd1cc7e4731ab5
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-22 23:05:06 -0700

    css for login and hike list pages

--- hike-list/hike-list.js
+++ hike-list/hike-list.js
@@ -7,7 +7,7 @@ const serchId = new URLSearchParams(window.location.search);
 const keyID = serchId.get('userId');
 const localVar = localStorage.getItem(keyID);
 
-console.log(localVar);
+// console.log(localVar);
 
 const quadrant = 'Northeast';
 
