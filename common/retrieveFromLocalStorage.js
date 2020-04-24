c9170355f251f25a71855e57992e18187ea4e714
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-23 17:29:37 -0700

    updating function files plus darn you travis

--- common/retrieveFromLocalStorage.js
+++
@@ -1,9 +0,0 @@
-//Takes in a KEY for localstorage and returns the matching parsed value
-export default function retrievefromLocalStorage(key) {
-    const json = localStorage.getItem(`${key}`);
-    let value = null;
-    if (json) {
-        value = JSON.parse(json);
-    }
-    return value;
-}
